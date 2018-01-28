f0141609f37e3672b27c1e51b40c661f1e6d62e4
Ben Chapman-Kish <ben.chapmankish@gmail.com>
2018-01-28 05:34:52 -0500

    Initial Commit

---
+++ DecorAssist/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DecorAssist
+//
+//  Created by Ben Chapman-Kish on 2018-01-28.
+//  Copyright © 2018 Big Poppa Productions. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DecorAssist/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DecorAssist
+//
+//  Created by Ben Chapman-Kish on 2018-01-28.
+//  Copyright © 2018 Big Poppa Productions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
